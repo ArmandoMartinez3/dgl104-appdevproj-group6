a32ba462dab12dde4d36349d9e8f29c6f2a2a94e
Armando Martinez <amartinezguillen@northislandcollege.ca>
2025-03-11 14:31:44 -0700

    Fisrt Commit

---
+++ gymapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('', views.home, name='home'), 
+    path('signup/', views.signup, name='signup'),
+    path('signin/', auth_views.LoginView.as_view(template_name='signin.html'), name='signin'),
+    path('signout/', auth_views.LogoutView.as_view(next_page='signin'), name='signout'),
+]
