fe299f9c145d80e58281997dff5fa5ec23949d16
Armando Martinez <amartinezguillen@northislandcollege.ca>
2025-04-05 14:11:30 -0700

    This should Collect Static Files

--- STMS/settings.py
+++ STMS/settings.py
@@ -51,6 +51,7 @@ LOGOUT_REDIRECT_URL = '/signin/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -130,6 +131,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "gymapp/static",
 ]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
