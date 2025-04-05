d112db0a8cc61dd109c40f49813bc2b331ac7358
Armando Martinez <amartinezguillen@northislandcollege.ca>
2025-04-05 15:39:22 -0700

    Create Superuser File Attempt 2

--- create_superuser.py
+++ create_superuser.py
@@ -6,21 +6,27 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'STMS.settings')
 django.setup()
 
 def create_superuser():
-    User = get_user_model()
-    username = os.getenv('DJANGO_SUPERUSER_USERNAME', 'admin')
-    email = os.getenv('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
-    password = os.getenv('DJANGO_SUPERUSER_PASSWORD', 'cheetos123')
+    try:
+        User = get_user_model()
+        username = os.getenv('DJANGO_SUPERUSER_USERNAME', 'admin')
+        email = os.getenv('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
+        password = os.getenv('DJANGO_SUPERUSER_PASSWORD', 'admin123')
 
-    if not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(
-            username=username,
-            email=email,
-            password=password,
-            user_type='admin'
-        )
-        print(f"Superusuario {username} creado exitosamente")
-    else:
-        print(f"El superusuario {username} ya existe")
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(
+                username=username,
+                email=email,
+                password=password,
+                user_type='admin',
+                phone='',
+                address=''
+            )
+            print(f"Superusuario {username} creado exitosamente")
+        else:
+            print(f"El superusuario {username} ya existe")
+    except Exception as e:
+        print(f"Error al crear el superusuario: {str(e)}")
+        raise
 
 if __name__ == "__main__":
     create_superuser() 
