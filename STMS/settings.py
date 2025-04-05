041e11b579212889ff32810de2cf6e64211e24d1
Armando Martinez <amartinezguillen@northislandcollege.ca>
2025-04-05 14:07:25 -0700

    This should fix the Settings File

--- STMS/settings.py
+++ STMS/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-xqexihcb&s56@&$4^1n(u63qutmgs6(#cu$h#_+d_hvvpj^xf8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dgl104-appdevproj-group6.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
